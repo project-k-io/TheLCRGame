86097d133549089c73b898e7fa82e10e36fe24fc
Alan Kharebov <ak61us@msn.com>
2021-10-14 15:27:52 -0700

    Added SimulatorView

--- ProjectK.Games.LCR/ProjectK.Games.LCR.Views/SimulatorView.xaml.cs
+++ ProjectK.Games.LCR/ProjectK.Games.LCR.Views/SimulatorView.xaml.cs
@@ -12,16 +12,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using ProjectK.Games.LCR.ViewModels;
 
-namespace ProjectK.Games.LCR.WinApp
+namespace ProjectK.Games.LCR.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SimulatorView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SimulatorView : UserControl
     {
-        public MainWindow()
+        public SimulatorView()
         {
             InitializeComponent();
         }
---
+++ ProjectK.Games.LCR/ProjectK.Games.LCR.WinApp/AppViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectK.Games.LCR.ViewModels;
+
+namespace ProjectK.Games.LCR.WinApp
+{
+    public class AppViewModel: SimulatorViewModel
+    {
+        public string AppName { get; set; }
+        public string Title { get; set; }
+        public string Version { get; set; }
+
+        public AppViewModel()
+        {
+            AppName = "The LCR Game";
+            Version = "1.0.0";
+            Title = $"{AppName} {Version}";
+        }
+    }
+}
